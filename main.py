ea476c8555adef3a7347d4f902c093a3ecd676be
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-03-26 00:30:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 # Environment configuration
 API_ID = 27999679
 API_HASH = "f553398ca957b9c92bcb672b05557038"
-BOT_TOKEN = "7647294451:AAHo8YWCZTnInsr24BTQSzCZu-SGuCuVf14"
+BOT_TOKEN = "7747531722:AAFgE11HA4SbfjOFWTVT3_Sp-xR8tBDJdeo"
 MAX_FILE_SIZE = 2000000000  # 2GB
 SUPPORTED_SUBTITLE_EXT = [".srt", ".ass", ".ssa", ".vtt"]
 TEMP_DIR = Path("temp")
