e503160c739168de43fa3aebbb5af0371cd4d502
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-03-26 16:57:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     API_HASH = "f553398ca957b9c92bcb672b05557038"
 
     #comma seperated user id of users who are allowed to use
-    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','5314733603,6022020248,5996829431,6136203777,7235663635').split(',')]
+    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','1423807625,1048110820').split(',')]
 
     DOWNLOAD_DIR = 'downloads'
